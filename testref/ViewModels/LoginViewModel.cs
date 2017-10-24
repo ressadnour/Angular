5b675a2e856a76be690c028773ab3bd981b177a2
ressadnour <ressadnour@gmail.com>
2017-10-24 11:28:03 +0200

    commit data

---
+++ testref/ViewModels/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testref.ViewModels
+{
+    public class AuthenticateViewModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+
+
+    }
+}
